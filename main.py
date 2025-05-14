869d3017ed1f38174b03890d8eed5d09724f8774
mcruzespinosa <147786403+mcruzespinosa@users.noreply.github.com>
2025-05-14 13:34:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ elif selected == "Registro de horas":
                 # Terminar proyecto
                 from datetime import datetime
 
-                fin = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                fin = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 inicio = st.session_state.inicio_proyecto
 
                 # Asegúrate de que 'inicio' sea una cadena
@@ -260,7 +260,7 @@ elif selected == "Registro de horas":
                 st.success(f"✅ Proyecto '{proyecto_seleccionado}' terminado correctamente.")
             else:
                 # Iniciar proyecto
-                inicio = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                inicio = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 insertar_registro(st.session_state.user, proyecto_seleccionado, inicio)
                 # Guardar ID del registro y estado de sesión
                 ultimo_registro = obtener_ultimo_registro(st.session_state.user, proyecto_seleccionado)
